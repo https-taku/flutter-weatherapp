23759d59be5097621c6a2ada5eeb7ad9a20e60a4
Takudzwa Nhawu <takudzwa.nhawu@students.uz.ac.zw>
2024-12-29 15:53:34 +0200

    first version

--- weatherapp/lib/pages/weather_page.dart
+++ weatherapp/lib/pages/weather_page.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:weatherapp/models/weather_models.dart';
 import 'package:weatherapp/service/weather_service.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -34,6 +35,34 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  //weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) {
+      return 'assets/sunny day.json';
+    }
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'fog':
+        return 'assets/cloudy.json';
+
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rainy day.json';
+
+      case 'thunderstorm':
+        return 'assets/thunderstorm.json';
+
+      case 'clear':
+      case 'sun':
+        return 'assets/sunny day.json';
+
+      default:
+        return 'assets/sunny day.json';
+    }
+  }
+
   //initial state
 
   @override
@@ -45,11 +74,27 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(title: const Text('Weather App')),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // city name
+
           Text(_weather?.cityName ?? 'Loading city...'),
+
+          // animation
+          Lottie.asset(
+            getWeatherAnimation(_weather?.mainCondition),
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+
+          // temperature
           Text('${_weather?.temperature.round()} Degrees Celcius'),
+
+          // weather condition
+          Text(_weather?.mainCondition ?? ''),
         ]),
       ),
     );
